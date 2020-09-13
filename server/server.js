27d2a1e30d567145afa5dafb198d3e40251c0842
MalkareddyJithender <jithendermalkareddy@gmail.com>
2020-09-14 00:30:33 +0530

    indecision app

--- server/server.js
+++ server/server.js
@@ -4,9 +4,8 @@ const app = express();
 const publicDirPath = path.join(__dirname,'..','public');
 const port = 3000 || process.env.PORT;
 
-
 app.use(express.static(publicDirPath));
 
 app.listen(port,() =>{
-    console.log(`server is up on port ${port}`);
+    console.log(`server is up `);
 });
